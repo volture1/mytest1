ba682fe6f8b65a346ad3c31e5d363cfb6f15478a
Tobias Östergren <ostergrentobias@gmail.com>
2018-11-21 14:06:34 +0100

    Added the project into the repo

---
+++ src/hkr/test/Main.java
@@ -0,0 +1,9 @@
+package hkr.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello this is the first version of the project");
+    }
+}
