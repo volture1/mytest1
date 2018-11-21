282f1e4f575fea0405e434cb2bd22d749581dd54
Tobias Östergren <ostergrentobias@gmail.com>
2018-11-21 14:49:26 +0100

    added a new line

--- src/hkr/test/Main.java
+++ src/hkr/test/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello this is the first version of the project");
         System.out.println("This is the second version of the project");
+        System.out.println("This adds a new line of code in the project." );
+        System.out.println("Another one!");
     }
 }
