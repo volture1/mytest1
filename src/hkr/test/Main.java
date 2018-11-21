8b68629e4ea1540e53615ee8d30f15b52690c2c6
Tobias Östergren <ostergrentobias@gmail.com>
2018-11-21 14:09:14 +0100

    Added a printline into main

--- src/hkr/test/Main.java
+++ src/hkr/test/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello this is the first version of the project");
+        System.out.println("This is the second version of the project");
     }
 }
